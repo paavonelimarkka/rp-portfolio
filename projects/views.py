d31c1e9a80111617cc6ec825e5c42e7738717f03
Paavo <paavo.nelimarkka@gmail.com>
2019-10-13 17:57:10 +0300

    add project views and template for projext index

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from projects.models import Project
 
-# Create your views here.
 def project_index(request):
     projects = Project.objects.all()
     context = {
@@ -9,3 +8,10 @@ def project_index(request):
     }
     return render(request, 'project_index.html', context)
     
+    
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
